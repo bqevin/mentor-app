b0d0362901d8131b55bfa3d7eab841bc8bc8ca90
Kevin Barasa <kevin.barasa001@gmail.com>
2017-02-06 16:08:42 +0300

    Body parser iko sasa

--- index.js
+++ index.js
@@ -1,6 +1,41 @@
+//Require all files
 var express = require('express');
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
 
+
+//Declaring Database Schema
+var Schema = mongoose.Schema;
+var ObjectId = Schema.ObjectId;
+var User = mongoose.model('User', new Schema(
+		// {
+		// 	id:ObjectId,
+		// 	name: String,
+		// 	age : Number,
+		// 	gender : String
+		// }
+));
+
+//Connecting the mongoose database
+mongoose.connect('mongodb://localhost/rest_api');
+
+
+//Init app
 var app =express();
+
+//ALL CONFIGURATIONS
+app.use(express.static(__dirname + '/public'));
+//Set the view engine
+app.set('view engine', 'html');
+
+//Have the HTML in the view source
+app.locals.pretty = true;
+
+//set body parser
+app.use(bodyParser.urlencoded({extended : true}));
+app.use(bodyParser.json());
+
+
 
 app.get('/', function(req, res){
   res.render('Random test');

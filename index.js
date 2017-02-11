aade90ef213d66aba70176237663d775a3ce0e3c
Kevin Barasa <kevin.barasa001@gmail.com>
2017-02-11 20:48:02 +0300

    Decided to just use ejs engine

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 //Require all files
 var express = require('express');
 var bodyParser = require('body-parser');
-var cons = require('consolidate');
-var swig = require('swig');
 var methodOverride = require('method-override');
+
+var passport = require('passport');
+var flash    = require('connect-flash');
+var morgan       = require('morgan');
+var cookieParser = require('cookie-parser');
+var session      = require('express-session');
 
 
 //Init app
@@ -18,10 +22,14 @@ var oneDay = 86400000;
 
 //ALL CONFIGURATIONS
 // view engine setup
-app.engine('html', cons.swig)
+//app.engine('html', cons.swig)
 
 app.use(express.static(__dirname + '/public', { maxAge: oneDay }));
-app.set('view engine', 'html');
+
+//Set  view engine
+app.set('view engine', 'ejs');
+
+//app.set('view engine', 'html');
 
 //Have the HTML in the view source
 app.locals.pretty = true;
@@ -35,8 +43,16 @@ app.use(bodyParser.json());
 app.use(methodOverride('X-HTTP-Method-Override')); 
 
 
+// required for passport
+app.use(session({ secret: 'notateacherbutanawakener' })); // session secret
+app.use(passport.initialize());
+app.use(passport.session()); // persistent login sessions
+app.use(flash()); // use connect-flash for flash messages stored in session
+
+
 // routes ==================================================
-require('./app/routes')(app); // configure our routes
+require('./app/routes')(app, passport); // load our routes and pass in our app and fully configured passport
+
 
 
 
